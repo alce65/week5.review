77199badba16da2692407fb6047d0827724547b9
Alejandro Cerezo <alce65@hotmail.es>
2023-08-21 13:31:19 +0200

    Initial commit

---
+++ src/components/app/app.tsx
@@ -0,0 +1,13 @@
+export function App() {
+  return (
+    <>
+      <header>
+        <h1>Counters</h1>
+      </header>
+      <main></main>
+      <footer>
+        <address>ISDI Coders</address>
+      </footer>
+    </>
+  );
+}
